460883a7ef6251442adf8c84c246a47ddfd988d4
luadham <luadham@gmail.com>
2021-12-15 16:25:45 +0200

    Splash Screen Added

---
+++ app/src/main/java/remote/jop/Control/MainActivity.java
@@ -0,0 +1,16 @@
+package remote.jop.Control;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import remote.jop.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
