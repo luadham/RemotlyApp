c0792a7f2f719346023f94f05b16b1a9a5bfd4ce
luadham <luadham@gmail.com>
2021-12-15 21:31:04 +0200

    Login UI Done

--- app/src/main/java/remote/jop/Control/SplashScreen.java
+++ app/src/main/java/remote/jop/Control/SplashScreen.java
@@ -20,7 +20,7 @@ public class SplashScreen extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                startActivity(new Intent(SplashScreen.this, MainActivity.class));
+                startActivity(new Intent(SplashScreen.this, UserLogin.class));
                 SplashScreen.this.finish();
             }
         }, DELAY);
---
+++ app/src/main/java/remote/jop/Control/UserLogin.java
@@ -0,0 +1,16 @@
+package remote.jop.Control;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import remote.jop.R;
+
+public class UserLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_login);
+    }
+}
