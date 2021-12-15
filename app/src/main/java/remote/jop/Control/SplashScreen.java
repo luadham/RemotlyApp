1b4b52326928ac91a7e3ffda1d6241b286bb9906
luadham <luadham@gmail.com>
2021-12-15 23:08:21 +0200

    Add Logo animation

--- app/src/main/java/remote/jop/Control/SplashScreen.java
+++ app/src/main/java/remote/jop/Control/SplashScreen.java
@@ -6,6 +6,7 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.animation.AnimationUtils;
 
 import remote.jop.R;
 
@@ -17,6 +18,7 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
--- app/src/main/java/remote/jop/Control/UserLogin.java
+++ app/src/main/java/remote/jop/Control/UserLogin.java
@@ -3,14 +3,24 @@ package remote.jop.Control;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 import remote.jop.R;
 
 public class UserLogin extends AppCompatActivity {
-
+    private ImageView logoImageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_login);
+        logoImageView = findViewById(R.id.logo_login_form);
+
+        startAnimations();
+
+    }
+
+    void startAnimations() {
+        logoImageView.startAnimation(AnimationUtils.loadAnimation(this, R.anim.bounce));
     }
 }
