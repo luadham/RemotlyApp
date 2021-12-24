b513cd9102524e326c2aebf0c20198827217e067
luadham <luadham@gmail.com>
2021-12-24 14:37:44 +0200

    Now User Can Login

--- app/src/main/java/remote/jop/Control/Login/UserLogin.java
+++ app/src/main/java/remote/jop/Control/Login/UserLogin.java
@@ -2,14 +2,22 @@ package remote.jop.Control.Login;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 import remote.jop.Control.MainUI.MainActivity;
 import remote.jop.Control.ResetPassword.ResetPassword;
@@ -17,25 +25,35 @@ import remote.jop.Control.SignUp.UserSignUp;
 import remote.jop.R;
 
 public class UserLogin extends AppCompatActivity implements View.OnClickListener {
+
     private ImageView logoImageView;
-    private TextView signUpButton, forgetPassword;
+    private TextView signUpButton;
+    private TextView forgetPassword;
     private EditText emailEditText;
+    private EditText pwdEditText;
     private Button loginButton;
+    private FirebaseAuth firebaseAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_login);
 
-        logoImageView = findViewById(R.id.logo_login_form);
-        signUpButton = findViewById(R.id.sign_up_login_form);
-        forgetPassword = findViewById(R.id.forget_password_login_form);
-        emailEditText = findViewById(R.id.email_text_login_form);
-        loginButton = findViewById(R.id.login_button);
+        firebaseAuth = FirebaseAuth.getInstance();
 
-        loginButton.setOnClickListener(this);
+        signUpButton = findViewById(R.id.sign_up_login_form);
         signUpButton.setOnClickListener(this);
+
+        forgetPassword = findViewById(R.id.forget_password_login_form);
         forgetPassword.setOnClickListener(this);
+
+        logoImageView = findViewById(R.id.logo_login_form);
+        emailEditText = findViewById(R.id.email_text_login_form);
+        pwdEditText = findViewById(R.id.password_text_login_form);
+
+        loginButton = findViewById(R.id.login_button);
+        loginButton.setOnClickListener(this);
 
     }
 
@@ -49,7 +67,7 @@ public class UserLogin extends AppCompatActivity implements View.OnClickListener
                 goToSignUpForm(view);
                 return;
             case R.id.login_button:
-                doLogin(view);
+                doLogin();
                 return;
         }
     }
@@ -66,9 +84,37 @@ public class UserLogin extends AppCompatActivity implements View.OnClickListener
     void startAnimations() {
         logoImageView.startAnimation(AnimationUtils.loadAnimation(this, R.anim.bounce));
     }
-    void doLogin(View view) {
-        finish();
-        startActivity(new Intent(this, MainActivity.class));
+    void doLogin() {
+        String email = emailEditText.getText().toString().trim();
+        String pwd = pwdEditText.getText().toString();
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            emailEditText.setError("You Must Enter E-mail");
+            emailEditText.requestFocus();
+            return;
+        }
+
+        if (email.isEmpty()) {
+            emailEditText.setError("Email Is Required");
+            emailEditText.requestFocus();
+            return;
+        }
+
+        if (pwd.isEmpty()) {
+            pwdEditText.setError("Password is Required");
+            pwdEditText.requestFocus();
+            return;
+        }
+
+        firebaseAuth.signInWithEmailAndPassword(email, pwd)
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        finish();
+                        startActivity(new Intent(UserLogin.this, MainActivity.class));
+                    } else {
+                        Toast.makeText(UserLogin.this, "Login Failed", Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
     @Override
     protected void onResume() {
