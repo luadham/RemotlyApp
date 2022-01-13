0e23525ae0c61c3c22ac9d84860859e1e05e720d
luadham <luadham@gmail.com>
2022-01-13 16:08:51 +0200

    Add Multible Job

--- app/src/main/java/remote/jop/Control/Company/CompanyPostJob.java
+++ app/src/main/java/remote/jop/Control/Company/CompanyPostJob.java
@@ -1,5 +1,7 @@
 package remote.jop.Control.Company;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -8,8 +10,12 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import Model.Company;
 import Model.Job;
@@ -58,7 +64,7 @@ public class CompanyPostJob extends AppCompatActivity implements View.OnClickLis
         job = getJob();
         job.setCompanyEmail(company.getEmail());
         firebaseDatabase = manager.getDatabaseReference().getReference(jops);
-        firebaseDatabase.setValue(job);
+        firebaseDatabase.push().setValue(job);
     }
     @Override
     public void onClick(View view) {
